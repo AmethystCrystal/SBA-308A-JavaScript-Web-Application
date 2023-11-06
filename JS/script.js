97cddb3bbbcb6dff068c4d619fcff3ba0f031366
AmethystCrystal <140659819+AmethystCrystal@users.noreply.github.com>
2023-11-06 17:11:42 -0500

    created a card to display my API data inside

--- JS/script.js
+++ JS/script.js
@@ -1,17 +1,33 @@
-const url = 'https://hobbies-by-api-ninjas.p.rapidapi.com/v1/hobbies';
-const options = {
-	method: 'GET',
-	headers: {
+async function fetchData() {
+	const url = 'https://famous-quotes4.p.rapidapi.com/random?category=all&count=2';
+	const options = {
+	  method: 'GET',
+	  headers: {
 		'X-RapidAPI-Key': '78559492c2mshae7e73506b12476p1382e7jsnfa33ce985a0e',
-		'X-RapidAPI-Host': 'hobbies-by-api-ninjas.p.rapidapi.com'
+		'X-RapidAPI-Host': 'famous-quotes4.p.rapidapi.com'
+	  }
+	};
+
+	try {
+	  const response = await fetch(url, options);
+	  const result = await response.json();
+	  const quotesContainer = document.querySelector('#quotes-container');
+
+	  if (Array.isArray(result) && result.length > 0) {
+		const firstQuote = result[0];
+		const quoteCard = document.createElement('div');
+		quoteCard.classList.add('quote-card');
+		quoteCard.innerHTML = `
+		  <strong>Author:</strong> ${firstQuote.author}<br><br>
+		  <blockquote>"${firstQuote.text}"</blockquote>
+		`;
+		quotesContainer.appendChild(quoteCard);
+	  } else {
+		console.error('No quotes found.');
+	  }
+	} catch (error) {
+	  console.error('Error:', error);
 	}
-};
+  }
 
-try {
-	const response = await fetch(url, options);
-	const result = await response.text();
-	console.log(result);
-} catch (error) {
-	console.error(error);
-}
-
+fetchData();
